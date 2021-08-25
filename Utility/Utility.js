2768c370e3fb79ebba857776fd622bd6484c34f4
name <varunbrahmane30@gmail.com>
2021-08-25 12:54:45 +0530

    [varun] added: Address Book contact created.

---
+++ Utility/Utility.js
@@ -0,0 +1,26 @@
+class Utility{
+    fName;
+    lName;
+    address;
+    city;
+    state;
+    zip;
+    phone;
+    email;
+
+    constructor(fName, lName, address, city, state, zip, phone, email){
+        this.fName = fName;
+        this.lName = lName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+        this.phone = phone;
+        this.email = email;
+    }
+}
+
+let data = new Utility("Varun", "Brahmane", "Loni", "Ahmadnagar", "Maharashtra", 413713, 7040391139, "b@gmail.com");
+console.log(data);
+
+module.exports = new Utility();
