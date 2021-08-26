edd6d32fd0aa0ae75014f6917222ae84aa7ab329
name <varunbrahmane30@gmail.com>
2021-08-26 15:10:28 +0530

    [varun] added: user validation for address book.

--- AddressBook/AddressBook.js
+++ AddressBook/AddressBook.js
@@ -1,2 +1,22 @@
 console.log("Welcome to address Book");
 const util = require('../Utility/Utility');
+const input = require('../Utility/Input');
+
+try {
+
+
+    let fName = input.getFirstName();
+    let lName = input.getLastName();
+    let address = input.getAddress();
+    let city = input.getCity();
+    let state = input.getState();
+    let zip = input.getZip();
+    let phone = input.getPhone();
+    let email = input.getEmail();
+   
+    const data = new util(fName, lName, address, city, state, zip, phone, email);
+    console.log(data.toString());
+    console.log(JSON.stringify(data))
+} catch (err) {
+    console.error(err);
+}
